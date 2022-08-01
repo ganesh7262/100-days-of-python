42a159b73497d1d182f77914b272dce527c32d08
Ganesh Shinde <ganeshshinde72620@gmail.com>
2022-08-01 21:24:10 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -1,3 +1,14 @@
-def conv(num:int)->float:
-    return (num*9/5)+32
+import tkinter
 
+
+window=tkinter.Tk()
+window.title("First GUI program")
+window.minsize(width=600,height=600)
+
+# Label
+my_label=tkinter.Label(text="My first Tkinter Label",font=("Arial",20,"bold"))
+my_label.pack(side="left")
+
+
+
+window.mainloop()
