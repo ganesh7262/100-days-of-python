58f7f170caa7f3befb540e22ce65f477937e540f
Ganesh Shinde <ganeshshinde72620@gmail.com>
2022-08-07 22:48:49 +0530

    exception handeling

--- 100 days of python/projects/nato/main.py
+++ 100 days of python/projects/nato/main.py
@@ -7,8 +7,16 @@ nato_data=pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict={row.letter:row.code for (index,row) in nato_data.iterrows() }
 
-name=input("Enter your Name: ")
+def create_nato():
+    name=input("Enter a Word: ")
+    nato_list=[nato_dict[letter.upper()] for letter in list(name)]
+    print(nato_list)
 
-nato_list=[nato_dict[letter.upper()] for letter in list(name)]
 
-print(nato_list)
+try:
+    create_nato()
+    
+except KeyError as err:
+    print('Sorry only letters from alphabets accepted')
+    create_nato()
+    
--- rough.py
+++ rough.py
@@ -1,37 +1,13 @@
-import tkinter
+fruits=['apple','orange','pear']
+
+def make_pie(index:int)->str:
+    try:
+        fruit=fruits[index]
+        print(f'{fruit} pie')
+    except IndexError as err:
+        print('No such fruit exists')
+    else:
+        print(f'{fruit} pie')
 
 
-window = tkinter.Tk()
-window.title("First GUI program")
-window.minsize(width=600, height=600)
-# Functions
-
-
-def button_test():
-    my_label.config(text="button works,challenge complete")
-
-
-def on_button_click():
-    my_label.config(text=inp.get())
-
-
-# Label
-my_label = tkinter.Label(text="New Text", font=("Arial", 20, "bold"))
-my_label.grid(column=0,row=0)
-
-
-# Button
-
-
-button1 = tkinter.Button(text="button1", command=on_button_click)
-button1.grid(column=1,row=1)
-
-
-buttomn2=tkinter.Button(text="button2")
-buttomn2.grid(column=2,row=0)
-# Input
-inp = tkinter.Entry()
-inp.grid(column=4,row=3)
-
-
-window.mainloop()
+make_pie(int(input('Enter the index: ')))
