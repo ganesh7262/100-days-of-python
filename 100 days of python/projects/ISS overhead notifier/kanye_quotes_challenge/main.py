44e9561d0db662950ba6fc5bfdd4d559c0fdb51b
Ganesh Shinde <ganeshshinde72620@gmail.com>
2022-08-24 11:37:20 +0530

    Update main.py

--- 100 days of python/projects/ISS overhead notifier/kanye_quotes_challenge/main.py
+++ 100 days of python/projects/ISS overhead notifier/kanye_quotes_challenge/main.py
@@ -16,12 +16,12 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file=r"C:\Users\ganes\OneDrive\Documents\GitHub\100-days-of-python\100 days of python\projects\ISS location finder\kanye_quotes_challenge\background.png")
+background_img = PhotoImage(file=r'C:\Users\ganes\OneDrive\Documents\GitHub\100-days-of-python\100 days of python\projects\ISS overhead notifier\kanye_quotes_challenge\background.png')
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="click kanye to get quote....", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file=r'C:\Users\ganes\OneDrive\Documents\GitHub\100-days-of-python\100 days of python\projects\ISS location finder\kanye_quotes_challenge\kanye.png')
+kanye_img = PhotoImage(file=r'C:\Users\ganes\OneDrive\Documents\GitHub\100-days-of-python\100 days of python\projects\ISS overhead notifier\kanye_quotes_challenge\kanye.png')
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
