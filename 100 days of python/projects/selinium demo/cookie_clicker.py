c24e10c53f8a58b480778c1a0112dd561fc93b42
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-04-20 20:35:14 +0530

    finished cookie cutter

--- 100 days of python/projects/selinium demo/cookie_clicker.py
+++ 100 days of python/projects/selinium demo/cookie_clicker.py
@@ -1,12 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 driver = webdriver.Chrome()
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+cookie = driver.find_element(by=By.XPATH, value='//*[@id="cookie"]')
+
+perk_vs_price = {
+    17: '//*[@id="buyCursor"]',
+    100: '//*[@id="buyGrandma"]',
+    500: '//*[@id="buyFactory"]',
+    2000: '//*[@id="buyMine"]',
+    7000: '//*[@id="buyShipment"]',
+    50000: '//*[@id="buyAlchemy lab"]',
+    1000000: '//*[@id="buyPortal"]',
+    123456789: '//*[@id="buyTime machine"]',
+}
 
 
-cookie = driver.find_element(by=By.XPATH, value='//*[@id="bigCookie"]')
+def buyPerk():
+    for i in range(len(perk_vs_price)):
+        if int(money.text) < list(perk_vs_price.keys())[i]:
+            perk = driver.find_element(
+                by=By.XPATH, value=perk_vs_price[list(perk_vs_price.keys())[i - 1]]
+            )
+            perk.click()
+            return
 
+
+money = driver.find_element(by=By.XPATH, value='//*[@id="money"]')
+
+start_time = time.time()
 while True:
+    if time.time() > start_time + 5:
+        print(driver.find_element(By.XPATH, value='//*[@id="cps"]'))
+        buyPerk()
+        start_time = time.time()
+
     cookie.click()
