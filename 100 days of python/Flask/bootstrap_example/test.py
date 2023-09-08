555c859b2355adffd2e5c045c9d2960db6f2f475
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-09-08 18:12:32 +0530

    bootstrap example website

--- 100 days of python/Flask/bootstrap_example/test.py
+++ 100 days of python/Flask/bootstrap_example/test.py
@@ -1,1 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def indexhtml():
+    return render_template("index.html")
+
+
+@app.route("/welcome")
+def welcome_page():
+    return render_template("welcome.html")
+
+
+if __name__ == "__main__":
+    app.run()
